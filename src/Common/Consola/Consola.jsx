4ee33833893a47ea1656ea01dedd5cfb247e3751
xIMet3 <joaquin19_91@hotmail.com>
2023-10-11 17:27:10 +0200

    [feat]Redesign of the delimitations of the central part of the console and creation of the temporary steering crosshead. // Rediseñado de las delimitaciones de la parte central de la consola y creacion de la cruceta de direccion provisional.

--- src/Common/Consola/Consola.jsx
+++ src/Common/Consola/Consola.jsx
@@ -1,50 +1,59 @@
 import React from "react";
-import './Consola.css';
+import "./Consola.css";
 
 export const Consola = () => {
-    return (
-        <div className="cuerpoConsola">
-            <div className="bordeArriba">
-                <div className="onOff">
-                    <div className="off">
-                        OFF ●
-                    </div>
-                    <div className="on">
-                    ● ON
-                    </div>
-
-                </div>
-            </div>
-            <div className="partePantalla">
-                <div className="pantallaExterior">
-                    <div className="margenTop"></div>
-                    <div className="pantallaCentral">
-                        <div className="margenIzq"></div>
-                        <div className="pantalla"></div>
-                        <div className="margenDer"></div>
-                    </div>
-                    <div className="margenBot"></div>
-                </div>
-            </div>
-            <div className="parteCentral">
-                <div className="parteCruceta"></div>
-                <div className="parteLogo"></div>
-                <div className="parteBotonera">
-                    <div className="vacio1"></div>
-                    <div className="botoneraAB">
-                        <div className="botoneraB"></div>
-                        <div className="botoneraA"></div>
-                    </div>
-                </div>
-            </div>
-            <div className="parteBaja">
-                <div className="vacio2"></div>
-                <div className="parteSelect"></div>
-                <div className="parteStart"></div>
-                <div className="parteAltavoz"></div>
-            </div>
-
+  return (
+    <div className="cuerpoConsola">
+      <div className="bordeArriba">
+        <div className="onOff">
+          <div className="off">OFF ●</div>
+          <div className="on">● ON</div>
         </div>
-
-    )
-}
+      </div>
+      <div className="partePantalla">
+        <div className="pantallaExterior">
+          <div className="margenTop"></div>
+          <div className="pantallaCentral">
+            <div className="margenIzq"></div>
+            <div className="pantalla"></div>
+            <div className="margenDer"></div>
+          </div>
+          <div className="margenBot"></div>
+        </div>
+      </div>
+      <div className="parteCentral">
+        <div className="parteLogo">
+          <div className="vacio3"></div>
+          <div className="logo"></div>
+          <div className="vacio1"></div>
+        </div>
+        <div className="parteBotonera">
+          <div className="parteCruceta">
+            <div className="cruceta">◦
+              <div className="fila">
+                <div className="boton arriba"></div>
+              </div>
+              <div className="fila"> ◦
+                <div className="boton izquierda"></div>
+                <div className="boton centro"></div>
+                <div className="boton derecha"></div>◦
+              </div>
+              <div className="fila2">
+                <div className="boton abajo"></div>◦
+              </div>
+            </div>
+          </div>
+          <div className="botoneraAYB">
+            <div className="vacio4"></div>
+          </div>
+        </div>
+      </div>
+      <div className="parteBaja">
+        <div className="vacio2"></div>
+        <div className="parteSelect"></div>
+        <div className="parteStart"></div>
+        <div className="parteAltavoz"></div>
+      </div>
+    </div>
+  );
+};
