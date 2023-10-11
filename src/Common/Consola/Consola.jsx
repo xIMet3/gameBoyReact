741128860e7a1d92e35ec0c49d0764508803f35f
xIMet3 <joaquin19_91@hotmail.com>
2023-10-11 05:16:31 +0200

    [feat]Creation of the upper border with the ON OFF and delimited the upper part containing the screen. Also delimited the screen. // Creacion del borde superior con el ON OFF y delimitada la parte alta que contiene la pantalla. Tambien delimitada la pantalla.

--- src/Common/Consola/Consola.jsx
+++ src/Common/Consola/Consola.jsx
@@ -4,6 +4,23 @@ import './Consola.css';
 export const Consola = () => {
     return (
         <div className="cuerpoConsola">
+            <div className="bordeArriba">
+                <div className="onOff">
+                    <div className="off">
+                        OFF ●
+                    </div>
+                    <div className="on">
+                    ● ON
+                    </div>
+
+                </div>
+            </div>
+            <div className="partePantalla">
+                <div className="pantallaExterior">
+                    
+                </div>
+
+            </div>
 
         </div>
 
