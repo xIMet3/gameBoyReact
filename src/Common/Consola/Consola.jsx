55b3254641b829c0dbd0f9486eba64dd4c905b25
xIMet3 <joaquin19_91@hotmail.com>
2023-11-04 02:02:23 +0100

    [feat]Added gifs of the games in the games list. Now when selecting a game it plays a small gif of the game in question. // Añadidos gifs de los juegos de la lista de juegos. Ahora al seleccionar un juego reproduce un pequeño gif del juego en cuestion.

--- src/Common/Consola/Consola.jsx
+++ src/Common/Consola/Consola.jsx
@@ -2,69 +2,64 @@ import React, { useState, useEffect } from "react";
 import "./Consola.css";
 import { useNavigate } from "react-router-dom";
 import encendidoGif from "../../../Gif/encendidoGif.gif";
+import superMarioWorldGif from "../../../Gif/superMarioWorld.gif";
+import tetrisGif from "../../../Gif/tetris.gif";
+import zeldaGif from "../../../Gif/zelda.gif";
 
 export const Consola = () => {
-  // Estado para controlar el encendido de la consola
   const [encendidoActivado, setEncendidoActivado] = useState(false);
-
-  // Estado para controlar el LED rojo
   const [ledRojo, setLedRojo] = useState(false);
-
-  // Estado para controlar la visibilidad del GIF
   const [gifVisible, setGifVisible] = useState(false);
-
-  // Lista de juegos disponibles
   const [juegos, setJuegos] = useState([
-    "Super Mario Land",
+    "Super Mario World",
     "Tetris",
     "The Legend of Zelda",
-    "Pokemon Rojo / Azul",
-    "Pokemon Amarillo",
-    "Donkey Kong",
-    "Wario Land 2",
-    "Metroid II",
-    "Mega Man V"
+    // Agrega los demás juegos aquí
   ]);
-
-  // Indice del juego seleccionado
   const [indiceJuego, setIndiceJuego] = useState(0);
-
-  // Hook de navegacion
+  const [juegosVisible, setJuegosVisible] = useState(true);
   const navigate = useNavigate();
 
-  // Funcion que maneja los clics en los botones
+  const gifs = {
+    "Super Mario World": superMarioWorldGif,
+    Tetris: tetrisGif,
+    "The Legend of Zelda": zeldaGif,
+    // Añade más juegos y GIFs aquí
+  };
+
+  const [gifJuego, setGifJuego] = useState(null);
+
   const handleClick = (tipo) => {
     if (tipo === "Encendido") {
       if (encendidoActivado) {
-        // Apaga la consola
         setEncendidoActivado(false);
         setLedRojo(false);
+        setGifJuego(null); // Agregado: Reiniciar gifJuego a null
         setGifVisible(false);
+        setJuegosVisible(true);
       } else {
-        // Enciende la consola
         setEncendidoActivado(true);
         setLedRojo(true);
+        setGifJuego(encendidoGif); // Agregado: Cambiar a encendidoGif
         setGifVisible(true);
-
-        // Despues de un tiempo, apaga el GIF
+        setJuegosVisible(false);
         setTimeout(() => {
           setGifVisible(false);
-        }, 2300);
+          setJuegosVisible(true);
+        }, 10000);
       }
     } else if (tipo === "Arriba") {
-      // Selecciona el juego anterior
       setIndiceJuego((prevIndice) =>
         prevIndice === 0 ? juegos.length - 1 : prevIndice - 1
       );
     } else if (tipo === "Abajo") {
-      // Selecciona el juego siguiente
       setIndiceJuego((prevIndice) =>
         prevIndice === juegos.length - 1 ? 0 : prevIndice + 1
       );
     }
+    console.log(encendidoActivado);
   };
 
-  // Efecto que cambia el juego seleccionado cada 5 segundos si la consola esta encendida
   useEffect(() => {
     const interval = setInterval(() => {
       if (encendidoActivado) {
@@ -72,16 +67,39 @@ export const Consola = () => {
           prevIndice === juegos.length - 1 ? 0 : prevIndice + 1
         );
       }
-    }, 5000);
+    }, 95000);
 
-    // Limpia el intervalo cuando el componente se desmonta
     return () => clearInterval(interval);
   }, [encendidoActivado, juegos]);
 
-  // Renderizado del componente
+  const reproducirGif = () => {
+    setGifVisible(true);
+    setJuegosVisible(false);
+
+    setTimeout(() => {
+      setGifVisible(false);
+      setJuegosVisible(true);
+    }, 2300);
+  };
+
+  const handleClickA = () => {
+    if (ledRojo && !gifVisible) {
+      const juegoSeleccionado = juegos[indiceJuego];
+      const gifJuego = gifs[juegoSeleccionado];
+
+      if (gifJuego) {
+        setGifJuego(gifJuego);
+        reproducirGif();
+      }
+    }
+  };
+
+  const handleClickB = () => {
+    setGifVisible(false);
+  };
+
   return (
     <div className="paginaEntera">
-      {/* Seccion del boton de encendido */}
       <div className="parteBotonEncendido">
         <div className="vacio9"></div>
         <div className="botonEncendido">
@@ -94,9 +112,7 @@ export const Consola = () => {
         </div>
       </div>
 
-      {/* Seccion del cuerpo de la consola */}
       <div className="cuerpoConsola">
-        {/* Borde superior */}
         <div className="bordeArriba">
           <div className="onOff">
             <div className="off">OFF ●</div>
@@ -104,17 +120,12 @@ export const Consola = () => {
           </div>
         </div>
 
-        {/* Parte de la pantalla */}
         <div className="partePantalla">
           <div className="pantallaExterior">
             <div className="margenTop"></div>
-
-            {/* Parte central de la pantalla */}
             <div className="pantallaCentral">
               <div className="margenIzq">
                 <div className="vacio8"></div>
-
-                {/* Zona del LED */}
                 <div className="zonaLed">
                   <div className="parteLed">
                     <div
@@ -125,20 +136,17 @@ export const Consola = () => {
                     <div className="conexion">) ) )</div>
                   </div>
                 </div>
-
-                {/* Zona de POWER */}
                 <div className="zonaPower">
                   <div className="power">POWER</div>
                 </div>
               </div>
 
-              {/* Pantalla de juegos */}
               <div className="pantalla">
-                {ledRojo && gifVisible && (
+                {ledRojo && gifVisible && gifJuego && (
                   <img
                     id="encendidoGif"
                     className="encendidoGif"
-                    src={encendidoGif}
+                    src={gifJuego}
                     alt="Encendido GIF"
                   />
                 )}
@@ -173,9 +181,7 @@ export const Consola = () => {
           </div>
         </div>
 
-        {/* Parte central de la consola */}
         <div className="parteCentral">
-          {/* Parte del logo de Nintendo */}
           <div className="parteLogo">
             <div className="vacio3"></div>
             <div className="logo">
@@ -184,9 +190,7 @@ export const Consola = () => {
             <div className="vacio1"></div>
           </div>
 
-          {/* Botonera de la consola */}
           <div className="parteBotonera">
-            {/* Parte de la cruceta */}
             <div className="parteCruceta">
               <div className="cruceta">
                 ◦
@@ -199,7 +203,6 @@ export const Consola = () => {
                   </div>
                 </div>
                 <div className="fila">
-                  {" "}
                   ◦
                   <div
                     className="boton izquierda"
@@ -230,7 +233,6 @@ export const Consola = () => {
               </div>
             </div>
 
-            {/* Parte de los botones A y B */}
             <div className="botoneraAYB">
               <div className="vacio4"></div>
               <div className="botoneraB">
@@ -243,7 +245,7 @@ export const Consola = () => {
               <div className="botoneraA">
                 <div className="vacio6"></div>
                 <div className="botonA">
-                  <div className="A" onClick={() => handleClick("A")}></div>
+                  <div className="A" onClick={handleClickA}></div>
                   <div className="letraA">A</div>
                 </div>
               </div>
@@ -251,11 +253,8 @@ export const Consola = () => {
           </div>
         </div>
 
-        {/* Parte inferior de la consola */}
         <div className="parteBaja">
           <div className="vacio2"></div>
-
-          {/* Parte del boton SELECT */}
           <div className="parteSelect">
             <div className="botonSelect">
               <div
@@ -266,7 +265,6 @@ export const Consola = () => {
             </div>
           </div>
 
-          {/* Parte del boton START */}
           <div className="parteStart">
             <div className="botonStart">
               <div className="start" onClick={() => handleClick("Start")}></div>
